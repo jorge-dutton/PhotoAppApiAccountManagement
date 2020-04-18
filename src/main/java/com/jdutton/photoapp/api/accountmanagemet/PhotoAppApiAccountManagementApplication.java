adb42de68a4aec6a8209492c1661698b5f471e02
Jorge Dutton <jorge.dutton@gmail.com>
2020-04-18 10:34:15 +0200

    First commit

---
+++ src/main/java/com/jdutton/photoapp/api/accountmanagemet/PhotoAppApiAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jdutton.photoapp.api.accountmanagemet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
+	}
+
+}
