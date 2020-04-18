f556fc7bd1502988203d4f935c4a698f832ac97b
Jorge Dutton <jorge.dutton@gmail.com>
2020-04-18 19:47:28 +0200

    Enable custom service indetification and por number

--- src/main/java/com/jdutton/photoapp/api/account/ui/controllers/AccountController.java
+++ src/main/java/com/jdutton/photoapp/api/account/ui/controllers/AccountController.java
@@ -1,5 +1,6 @@
 package com.jdutton.photoapp.api.account.ui.controllers;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/account")
 public class AccountController {
+    
+    private final Environment env;
+    
+        
+    public AccountController(Environment env) {
+	super();
+	this.env = env;
+    }
+
+
 
     @GetMapping("/status/check")
     public String status() {
-	return "Working!";
+	return "Working! Port: " + env.gerProperty("");
     }
 }
